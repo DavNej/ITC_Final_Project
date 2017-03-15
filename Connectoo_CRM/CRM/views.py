690ad9ebb6b8c790cd5d88b0507083838bc0d759
DavNej <david.nejar@gmail.com>
2017-03-15 11:07:35 +0200

    commit for sync

--- Connectoo_CRM/CRM/views.py
+++ Connectoo_CRM/CRM/views.py
@@ -7,19 +7,6 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 @login_required
 # @user_passes_test(lambda u: u.groups.filter(name='Manager').exists())
 def index(request):
-<<<<<<< HEAD
-    # school = School.objects.order_by('name')
-    # grade_list = Grade.objects.order_by('teacher')
-    # children = Child.objects.order_by('last_name')
-    # context = {
-    # 'grade_list': grade_list,
-    # 'children': children,
-    # 'school': school,
-    # }
-    return render(request, 'CRM/index.html')#, context)
-
-
-=======
     school = KGardens.objects.order_by('name')
     group = Groups.objects.order_by('name')
     children = Kids.objects.order_by('last_name')
@@ -29,7 +16,6 @@ def index(request):
     'school': school,
     }
     return render(request, 'CRM/index.html', context)
->>>>>>> 429b90a933ecef88849d7fcbe2a2f1125a9aca06
 
 def schools_table(request):
     schools = KGardens.objects.order_by('name')
@@ -54,12 +40,6 @@ def classes_per_school(request, k_garden_id):
     }
     return render(request, 'CRM/classes.html', context)
 
-<<<<<<< HEAD
-def children(request):
-    children = Kids.objects.all()
-    return render(request, 'CRM/children.html', {'children': children})
-
-
 def gallery(request):
     pictures = KidPhotos.objects.order_by('updated_at')
     return render(request, 'CRM/gallery.html', {'pictures': pictures})
@@ -74,11 +54,7 @@ def gallery_per_class(request):
     pics = KidPhotos.objects.all()
     return render(request, 'CRM/class_gallery.html', {'class': class_name, 'pics': pics})
 
-def children_per_class(request, k_garden_id, group_id):
-    school = KGardens.objects.get(id = k_garden_id)
-=======
 def children_per_class(request, group_id):
->>>>>>> 429b90a933ecef88849d7fcbe2a2f1125a9aca06
     group = Groups.objects.get(id = group_id)
     children = Kids.objects.filter(group_id = group_id)
     context = {
@@ -89,9 +65,6 @@ def children_per_class(request, group_id):
 
 ###################################################################
 
-def gallery(request):
-    # pictures = Picture.objects.order_by('event')
-    return render(request, 'CRM/gallery.html')#, {'pictures': pictures})
 
 def schools(request):
     schools = KGardens.objects.order_by('name')
