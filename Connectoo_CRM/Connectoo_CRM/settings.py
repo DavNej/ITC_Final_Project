02c1f74f339b6f5fbf6a1520f58edefc47ed069a
listgaus <listgaus@gmail.com>
2017-03-15 15:37:45 +0200

    syncing galleries

--- Connectoo_CRM/Connectoo_CRM/settings.py
+++ Connectoo_CRM/Connectoo_CRM/settings.py
@@ -87,11 +87,7 @@ DATABASES = {
         'PORT': '3306',
         } 
     }
-<<<<<<< HEAD
 
-
-=======
->>>>>>> dfb0ce3cc43093c90b9ed616be140924a362d19c
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
