f3bc469fbfe344672571ac3933d0f4664d070871
Valentine Ganansia <valentineganansia@gmail.com>
2017-03-19 16:46:02 +0200

    update

--- Connectoo_CRM/assets/custom/js/gallery.js
+++ Connectoo_CRM/assets/custom/js/gallery.js
@@ -4,31 +4,31 @@ var myAlbum = {};
 myAlbum.cardArr= [];
 
 myAlbum.toCard = function() {
-	// console.log(myAlbum.cardArr);
-	// myAlbum.a = $(this);
+	console.log(myAlbum.cardArr);
+	myAlbum.a = $(this);
 		myAlbum.image = $(this).closest('a').find('img').attr('src');
 	console.log("sel image", myAlbum.image);
 
 		console.log('test')
 		myAlbum.cardArr.push(myAlbum.image);
-		// myAlbum.cardArr.push("test");
-	// console.log("cart", myAlbum.cardArr);
+		myAlbum.cardArr.push("test");
+	console.log("cart", myAlbum.cardArr);
 
-	// localStorage.setItem("imgData", JSON.stringify(myAlbum.cardArr));
-	// console.log(myAlbum.cardArr);
-	// $(".btn.sbold.green.add.card").unbind("click").bind( "click", myAlbum.toCard);
+	localStorage.setItem("imgData", JSON.stringify(myAlbum.cardArr));
+	console.log(myAlbum.cardArr);
+	$(".btn.sbold.green.add.card").unbind("click").bind( "click", myAlbum.toCard);
 };
 
 
 
 //
-// myAlbum.albumArr=[];
-// myAlbum.selectToAlbum = function() {
-// 	myAlbum.a = $(this);
-// 	var image = ($(this).closest('a').find('img').attr('src'));
-// 	myAlbum.albumArr.push(image);
-// 	console.log(myAlbum.albumArr);
-// };
+myAlbum.albumArr=[];
+myAlbum.selectToAlbum = function() {
+	myAlbum.a = $(this);
+	var image = ($(this).closest('a').find('img').attr('src'));
+	myAlbum.albumArr.push(image);
+	console.log(myAlbum.albumArr);
+};
 
 
 
@@ -42,20 +42,20 @@ myAlbum.toCard = function() {
 // 	image.addClass("zoom");
 //     };
 
-// myAlbum.orderIt=function(){
-// 	myAlbum.b=$(this);
-// 	var image = ($(this).closest('a').find('img').attr('src'));
-// 	console.log(myAlbum.albumArr);
-// 	console.log(myAlbum.cardArr);
-// };
+myAlbum.orderIt=function(){
+	myAlbum.b=$(this);
+	var image = ($(this).closest('a').find('img').attr('src'));
+	console.log(myAlbum.albumArr);
+	console.log(myAlbum.cardArr);
+};
 
 // myAlbum.saveProject=function(){
 
 // }
 myAlbum.start=function(){
 	$(".btn.sbold.green.add.card").bind( "click", myAlbum.toCard);
-	// $(".btn.btn-circle.green.btn-sm.album").unbind("click").bind( "click", myAlbum.selectToAlbum);
-	// $(".order").unbind("click").bind( "click", myAlbum.orderIt);
+	$(".btn.btn-circle.green.btn-sm.album").unbind("click").bind( "click", myAlbum.selectToAlbum);
+	$(".order").unbind("click").bind( "click", myAlbum.orderIt);
 	// $(".cbp-item.web-design.graphic.print.motion").unbind("click").bind("click",myAlbum.zoomPicture);
 	// $(".zoom").hide();
 };
