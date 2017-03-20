cbb889df024ca7c5913d2ccb8e52c3ca177a69be
DavNej <david.nejar@gmail.com>
2017-03-20 18:38:57 +0200

    add to album function

--- Connectoo_CRM/assets/custom/js/gallery.js
+++ Connectoo_CRM/assets/custom/js/gallery.js
@@ -1,33 +1,42 @@
 var myAlbum = {};
 
-myAlbum.cardArr= [];
+// myAlbum.cardArr= [];
 
-myAlbum.toCard = function() {
-	console.log(myAlbum.cardArr);
-	myAlbum.a = $(this);
-	myAlbum.image = $(this).closest('a').find('img').attr('src');
-	console.log("sel image", myAlbum.image);
+// myAlbum.toCard = function() {
+// 	console.log(myAlbum.cardArr);
+// 	myAlbum.a = $(this);
+// 	myAlbum.image = $(this).closest('a').find('img').attr('src');
+// 	console.log("sel image", myAlbum.image);
 
-		console.log('test')
-		myAlbum.cardArr.push(myAlbum.image);
-		myAlbum.cardArr.push("test");
-	console.log("cart", myAlbum.cardArr);
+// 		console.log('test')
+// 		myAlbum.cardArr.push(myAlbum.image);
+// 		myAlbum.cardArr.push("test");
+// 	console.log("cart", myAlbum.cardArr);
 
-	localStorage.setItem("imgData", JSON.stringify(myAlbum.cardArr));
-	console.log(myAlbum.cardArr);
-	$(".btn.sbold.green.add.card").unbind("click").bind( "click", myAlbum.toCard);
-};
-
+// 	console.log(myAlbum.cardArr);
+// 	$(".btn.sbold.green.add.card").unbind("click").bind( "click", myAlbum.toCard);
+// };
 
 
-//
-myAlbum.albumArr=[];
+
 myAlbum.selectToAlbum = function() {
-	myAlbum.a = $(this);
-	var image = ($(this).closest('a').find('img').attr('src'));
-	myAlbum.albumArr.push(image);
-	console.log(myAlbum.albumArr);
+
+	myAlbum.image = $(this).closest('a').find('img').attr('src');
+	myAlbum.albumArr.push(myAlbum.image);
+	localStorage.setItem("imgData", JSON.stringify(myAlbum.albumArr));
 };
+
+myAlbum.removeFromAlbum = function() {
+	myAlbum.image = $(this).closest('a').find('img').attr('src');
+
+	myAlbum.albumArr = jQuery.grep(y, function(value) {
+  		return value != removeItem;
+	});
+
+	localStorage.setItem("imgData", JSON.stringify(myAlbum.albumArr));
+};
+
+
 
 
 
@@ -49,15 +58,14 @@ myAlbum.selectToAlbum = function() {
 // };
 
 myAlbum.start=function(){
-	$(".btn.sbold.green.add.card").bind( "click", myAlbum.toCard);
 	$(".btn.btn-circle.green.btn-sm.album").unbind("click").bind( "click", myAlbum.selectToAlbum);
-	$(".order").unbind("click").bind( "click", myAlbum.orderIt);
-	// $(".cbp-item.web-design.graphic.print.motion").unbind("click").bind("click",myAlbum.zoomPicture);
+	// $(".order").unbind("click").bind( "click", myAlbum.orderIt);
 	// $(".zoom").hide();
 };
 
 
 $(document).ready(function() {
     myAlbum.start();
+	myAlbum.albumArr = JSON.parse(localStorage.getItem('imgData'));
 });
 
