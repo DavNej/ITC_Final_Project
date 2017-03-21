250f85f96356b75bec883356af47fb17d268708c
daniellanev <nevies.daniella@gmail.com>
2017-03-21 19:20:02 +0200

    attendances

--- Connectoo_CRM/assets/custom/js/attendances.js
+++ Connectoo_CRM/assets/custom/js/attendances.js
@@ -1,9 +1,40 @@
-// var attendances = {};
+var attendances = {};
 
-// attendances.changeColour = function() {
-//     attendances.change = $(this).closest('tr').find('td .label.label-sm.label-default.checkin');
-//     attendances.change.removeClass('label label-sm label-default checkin');
-//     attendances
-// }
+attendances.changeToAbsent = function() {
+    attendances.change = $(this).closest('tr').find('td .label.label-sm.label-default.checkin');
+    attendances.change.removeClass('label label-sm label-default checkin');
+    attendances.change.addClass('label label-sm label-not-arrive not');
+    attendances.change.html('Absent');
+    attendances.numAbsent = $('td .label.label-sm.label-not-arrive.not').length;
+    attendances.numCheckin = $('td .label.label-sm.label-default.checkin').length;
+    $('.number.absent').html(attendances.numAbsent);
+    $('.number.checkin').html(attendances.numCheckin);
+    
+};
 
-// $('.button.check-in').onclick(changeColour);
+attendances.changeToCheckin = function() {
+    attendances.change = $(this).closest('tr').find('td .label.label-sm.label-not-arrive.not');
+    attendances.change.removeClass('label label-sm label-not-arrive not');
+    attendances.change.addClass('label label-sm label-default checkin');
+    attendances.change.html('Check in');
+    attendances.numAbsent = $('td .label.label-sm.label-not-arrive.not').length;
+    attendances.numCheckin = $('td .label.label-sm.label-default.checkin').length;
+    $('.number.absent').html(attendances.numAbsent);
+    $('.number.checkin').html(attendances.numCheckin);
+    
+};
+
+attendances.numAbsent = $('td .label.label-sm.label-not-arrive.not').length;
+attendances.numCheckin = $('td .label.label-sm.label-default.checkin').length;
+
+$(document).ready(
+    function(){
+        $('.notarrive').bind("click", attendances.changeToAbsent);
+        $('.check-in').bind("click", attendances.changeToCheckin);
+        console.log(attendances.numAbsent);
+        $('.number.absent').html(attendances.numAbsent);
+        $('.number.checkin').html(attendances.numCheckin);
+    }
+);
+
+
