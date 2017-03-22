7ee7f64d00af4d19ed671d7ebef18d16cc376a0d
daniellanev <nevies.daniella@gmail.com>
2017-03-22 14:02:28 +0200

    parent view and pics

--- Connectoo_CRM/Connectoo_CRM/urls.py
+++ Connectoo_CRM/Connectoo_CRM/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('CRM.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('Accounts.urls')),
 ]
+# + static(settings.STATIC_URL, document_root = settings.MEDIA_ROOT)
