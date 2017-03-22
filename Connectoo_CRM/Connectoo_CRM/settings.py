f33d1dcebaa9444f6fd01070c5ef27bbf0bc9863
Valentine Ganansia <valentineganansia@gmail.com>
2017-03-22 14:54:43 +0200

    children per school

--- Connectoo_CRM/Connectoo_CRM/settings.py
+++ Connectoo_CRM/Connectoo_CRM/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'connectoo_CRM',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'caroline5526V',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         }
